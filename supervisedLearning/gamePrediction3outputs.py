113e0af5aa0237af0274ebb56b28382c13d9a513
Amn8189 <aydenmunga@gmail.com>
2024-11-08 18:51:59 +0300

    streetlights

--- supervisedLearning/gamePrediction3outputs.py
+++ supervisedLearning/gamePrediction3outputs.py
@@ -1,5 +1,5 @@
 # Input
-number_fans = 50
+number_fans = 0.5
 
 # Outputs
 target_win = 0.6
@@ -8,45 +8,41 @@ target_lose = 0.1
 outputs = target_win, target_draw, target_lose
 
 # Weights
-weights = [0.7,  0.6, 11]
+weights = [0.7, 0.6, -0.2]
 
 def neuralNetwork(weights, input):
                 #win draw lose
     prediction = [0, 0,   0]
-    prediction[0] = weights[0] * input # Win
-    prediction[1] = weights[1] * input # Draw
-    prediction[2] = weights[2] * input # Lose
+    for i in range(len(prediction)):
+        prediction[i] = weights[i] * input
     return prediction
 
 def calcErrors(predictions, targets):
             #win draw lose
     errors = [0, 0,   0]
-    errors[0] = (predictions[0] - targets[0]) ** 2 # Win
-    errors[1] = (predictions[1] - targets[1]) ** 2 # Draw
-    errors[2] = (predictions[2] - targets[2]) ** 2 # Lose
+    for i in range(len(errors)):
+        errors[i] = (predictions[i] - targets[i]) ** 2
     return errors
 
 def calcDeltas(predictions, targets):
             #win draw lose
     deltas = [0, 0,   0]
-    deltas[0] = (predictions[0] - targets[0]) # Win
-    deltas[1] = (predictions[1] - targets[1]) # Draw
-    deltas[2] = (predictions[2] - targets[2]) # Lose
+    for i in range(len(deltas)):
+        deltas[i] = predictions[i] - targets[i]
     return deltas
 
 def calcWeightDeltas(input, deltas):
     weightDelta = [0, 0, 0]
-    weightDelta[0] = input * deltas[0]
-    weightDelta[1] = input * deltas[1]
-    weightDelta[2] = input * deltas[2]
+    for i in range(len(weightDelta)):
+        weightDelta[i] = input * deltas[i]
     return weightDelta
 
-lr = 0.001
+lr = 0.1
 def changeWeight(weights, weightDelta):
     for i in range(len(weights)):
         weights[i] = weights[i] - (weightDelta[i] * lr)
 
-for i in range(1000):
+for i in range(2000):
     ai_pred = neuralNetwork(weights, number_fans)
     print("AI prediction: " + str(ai_pred))
 
---
+++ supervisedLearning/streetLightProb2.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+inputs = np.array([
+    [1, 0, 1],
+    [0, 1, 1],
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 1, 1],
+    [1, 0, 1]
+])
+
+outputs = np.array([0, 1, 0, 1, 1, 0])
+
+weights1 = np.zeros(shape=(4, 3))
+print(weights1)
+weights2 = np.zeros(shape=(1, 4))
+
+myInput = inputs[0]
+myOutput = outputs[0]
+
+for i in range(20):
+    for j in range(len(inputs)):
+        pred = inputs[j].dot(weights)
+        error = (pred - outputs[j]) ** 2
+        print("ERROR is: " + str(error))
+        delta = pred - outputs[j]
+        print("DELTA is: " + str(delta))
+        weightDelta = delta * inputs[j]
+        weightDelta = weightDelta * 0.1 #lr
+        weights -= weightDelta
+
+pred = inputs[0].dot(weights)
+print("Final Prediction: " + str(pred))
+print("Final Weights: " + str(weights))
