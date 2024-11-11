0af260c904ae28a48b30477287734c03ad0c8355
Amn8189 <aydenmunga@gmail.com>
2024-11-11 19:04:35 +0300

    Error with Weights

---
+++ supervisedLearning/quickEx(transpose).py
@@ -0,0 +1,8 @@
+import numpy as np
+
+w = np.array( [
+    [1, 2, 12],
+    [3, 4, 10]
+])
+
+print(w.T)# Transpose - reverses rose with columns
--- supervisedLearning/streetLightProb2.py
+++ supervisedLearning/streetLightProb2.py
@@ -11,13 +11,21 @@ inputs = np.array([
 
 outputs = np.array([0, 1, 0, 1, 1, 0])
 
-weights1 = np.zeros(shape=(4, 3))
+weights1 = np.zeros(shape=(3, 4))
 print(weights1)
-weights2 = np.zeros(shape=(1, 4))
+weights2 = np.zeros(shape=(4, 1))
 
-h_out = weights1.dot(inputs[0])
-pred = weights2.dot(h_out)
-error = pred - outputs[0] ** 2
-delta = pred - outputs[0] 
+for i in range(100):
+    for j in range(len(inputs)):
+        h_out = inputs[j].dot(weights1)
+        pred = h_out.dot(weights2)
+        error = pred - outputs[j] ** 2
+        delta = pred - outputs[j]
+        delta_hidden = weights2.dot(delta.T)# T = Transpose aka reversing
 
-print("error is" + str(error))
+        lr = 0.01
+        #update weights
+        weights2 -= h_out.T.dot(delta) * lr
+        weights1 -= inputs.T.dot(delta_hidden) * lr
+    
+        print("error is" + str(error))
