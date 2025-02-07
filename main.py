e114a0e8cb8af1a50688214617faa9aa531a8667
Amn8189 <123394314+Amn8189@users.noreply.github.com>
2025-02-07 18:55:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, redirect
 from reshape import mnist_preprocess
 from flask import jsonify
 app = Flask(__name__)
