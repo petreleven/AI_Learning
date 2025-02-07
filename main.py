01b4bdd6e0b963444a9eb2ea0644030133917ba8
Amn8189 <123394314+Amn8189@users.noreply.github.com>
2025-02-07 19:07:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from reshape import mnist_preprocess
 from flask import jsonify
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        return redirect(url, code=301)
+#@app.before_request
+#def before_request():
+#    if not request.is_secure:
+#        url = request.url.replace('http://', 'https://', 1)
+#        return redirect(url, code=301)
 
 @app.route("/")
 def homepage():
